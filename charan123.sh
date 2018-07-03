6a8e5fa403025a1e346979c0d2c11a3ae86b0ebc
sam <40433567+sathwik35@users.noreply.github.com>
2018-07-03 15:43:59 -0400

    change

--- charan123.sh
+++ charan123.sh
@@ -4,7 +4,7 @@ age=25
 
 if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
 then 
-   echo "this is valid age"
+   echo "this is true valid age"
 else
-   echo "this ..is hhhnot at all valid age"
+   echo "this not at all valid age"
 fi 
